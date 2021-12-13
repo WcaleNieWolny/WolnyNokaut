1a25a7bf6de42f51760459eeb7028a3d05a9e423
WcaleNieWolny <isupermichael007@gmail.com>
2021-12-13 19:20:12 +0100

    optimize imports and set type to "data class" (KnockedPlayer)

--- src/main/kotlin/pl/wolny/wolnynokaut/knocked/KnockedPlayer.kt
+++ src/main/kotlin/pl/wolny/wolnynokaut/knocked/KnockedPlayer.kt
@@ -1,31 +1,10 @@
 package pl.wolny.wolnynokaut.knocked
 
-import com.comphenix.protocol.PacketType
-import com.comphenix.protocol.ProtocolLibrary
-import com.comphenix.protocol.events.PacketContainer
-import com.comphenix.protocol.wrappers.EnumWrappers
-import com.comphenix.protocol.wrappers.EnumWrappers.EntityPose
-import com.comphenix.protocol.wrappers.WrappedDataWatcher
-import com.comphenix.protocol.wrappers.WrappedDataWatcher.WrappedDataWatcherObject
-import net.kyori.adventure.title.Title
-import org.bukkit.Bukkit
-import org.bukkit.GameMode
-import org.bukkit.Location
-import org.bukkit.NamespacedKey
 import org.bukkit.entity.Player
-import org.bukkit.persistence.PersistentDataType
-import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
-import pl.wolny.wolnynokaut.NokautConfig
-import pl.wolny.wolnynokaut.utils.ComponentUtils
-import pl.wolny.wolnynokaut.utils.EffectUtils
-import pl.wolny.wolnynokaut.utils.FakeGameModeUtils
-import java.time.Duration
-import kotlin.math.pow
-import kotlin.math.sqrt
 
 
-class KnockedPlayer(val player: Player, val knockedBossbar: KnockedBossbar, dedTime: Short) {
+data class KnockedPlayer(val player: Player, val knockedBossbar: KnockedBossbar, val dedTime: Short) {
     lateinit var timeRunnable: BukkitRunnable
     var state: KnockedState = KnockedState.GROUND
     var time = dedTime
